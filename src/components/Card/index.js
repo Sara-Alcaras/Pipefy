cd3388f2f7ef7688591f353a8ca95f284a90fada
Sara-Alcaras <saraalcaras401@gmail.com>
2020-06-16 19:04:38 -0300

    styling card, list, board and header

---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Card() {
+  return (
+   <Container>
+     <header>
+       <label color="#7159c1"/>
+     </header>
+     <p>Fazer migração completa de servidor</p>
+     <img src="https://api.adorable.io/avatars/285/abott@adorable.png" alt="Avatar" />
+   </Container>
+  );
+}
