c91ff867d45acdc44988319aefdebb3eed472690
Sara-Alcaras <saraalcaras401@gmail.com>
2020-06-16 20:30:55 -0300

    add dnd

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useDrag } from 'react-dnd';
 
-import { Container } from './styles';
+import { Container, Label } from './styles';
 
 export default function Card( {data} ) {
+  const [{ isDragging }, dragRef] = useDrag({
+    item: { type: 'CARD' },
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+
+    }),
+
+  });
   return (
-   <Container>
+   <Container ref={dragRef}>
      <header>
-      {data.labels.map(label => <label key={label} color={label} />)}
+      {data.labels.map(label => <Label key={label} color={label} />)}
      </header>
      <p>{data.content}</p>
      {data.user && <img src={data.user} alt="Avatar"/> }
